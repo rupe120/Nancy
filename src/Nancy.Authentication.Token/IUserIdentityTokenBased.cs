5b4f2d3df9081e9fe70420e8f1f89d1b34e73034
Joshua Russo <jrusso@innovativesol.com>
2015-02-02 13:47:20 -0500

    added the remaining logic for cookie fallback token validation

---
+++ src/Nancy.Authentication.Token/IUserIdentityTokenBased.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Nancy.Security;
+
+namespace Nancy.Authentication.Token
+{
+    /// <summary>
+    /// An extension of the user identity interface for tracking where the token came from
+    /// </summary>
+    public interface IUserIdentityTokenBased : IUserIdentity
+    {
+        /// <summary>
+        /// Was the token was found in the header as opposed to a cookie?
+        /// </summary>
+        bool TokenFoundInHeader { get; set; }
+    }
+}
